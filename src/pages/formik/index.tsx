d7fcb6244ecf2ab33bce11bbaf3bd5ccf188f950
Evianna <xinyu721.k@gmail.com>
2025-01-20 15:57:14 +0800

    add formik

--- src/pages/formik/index.tsx
+++ src/pages/formik/index.tsx
@@ -1,18 +1,24 @@
-import { Formik } from "formik";
+import { Formik, ErrorMessage } from "formik";
 import PageLayout from "@/components/PageLayout";
 import { ReactElement } from "react";
+import { useState } from "react";
 
 const initialValues = {
   name: "",
   email: "",
-  grander: "",
-  birthday: "",
-  city: "",
-  dist: "",
-  address: "",
 };
 
 export default function Page() {
+  const handleValidate = (values: typeof initialValues) => {
+    const _errors: Record<string, string> = {};
+    if (!values.name) {
+      _errors.name = "Required!";
+    }
+    if (!values.email) {
+      _errors.email = "Required!";
+    }
+    return _errors;
+  };
   return (
     <>
       <h2 className="font-bold text-3xl px-2">[Formik]</h2>
@@ -26,9 +32,51 @@ export default function Page() {
         </a>
         <Formik
           initialValues={initialValues}
-          validate={() => {}}
+          validate={handleValidate}
           onSubmit={() => {}}
-        ></Formik>
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+          }) => (
+            <div className="p-8 mt-4 bg-yellow-300/40 rounded-[20px]">
+              <div className="px-2 font-bold text-xl pb-2 border-b-2 border-slate-400">
+                Custom Form
+              </div>
+              <form className="grid grid-cols-1 gap-4 py-4 px-2">
+                <label className="flex">
+                  <div className="mr-4 w-20">Name:</div>
+                  <input
+                    type="text"
+                    name="name"
+                    className="border"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.name}
+                  />
+                  <div className="ml-2 text-red-600">{errors.name}</div>
+                </label>
+                <label className="flex">
+                  <div className="mr-4 w-20">Email:</div>
+                  <input
+                    type="text"
+                    name="email"
+                    className="border"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                  />
+                  <div className="ml-2 text-red-600">{errors.email}</div>
+                </label>
+              </form>
+            </div>
+          )}
+        </Formik>
       </div>
     </>
   );
